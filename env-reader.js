a820cbd1bcf701a29ed1f6634f2b1e6cc5edb1ea
Martin Danielsson <martin@danielsson.de>
2016-09-14 14:57:26 +0200

    Haufe-Lexware/wicked.haufe.io#5: Change initial configuration to make localhost setups easier. Introduced PORTAL_CONFIG_BASE.

--- env-reader.js
+++ env-reader.js
@@ -14,13 +14,16 @@ var envReader = function () { };
 
 envReader.resolveStaticConfig = function() {
     var configDir;
-    if (process.env.PORTAL_API_STATIC_CONFIG)
+    if (process.env.PORTAL_API_STATIC_CONFIG) {
         configDir = process.env.PORTAL_API_STATIC_CONFIG;
-    else
+    } else if (process.env.PORTAL_CONFIG_BASE) {
+        configDir = path.join(process.env.PORTAL_CONFIG_BASE, 'static');
+    } else {
         configDir = '/var/portal-api/static';
+    }
     var globalFile = path.join(configDir, 'globals.json');
     if (!fs.existsSync(globalFile))
-        throw new Error('Could not resolve static configuration path; tried PORTAL_API_STATIC_CONFIG and /var/portal-api/static.');
+        throw new Error('Could not resolve static configuration path; tried PORTAL_API_STATIC_CONFIG, checked PORTAL_CONFIG_BASE and /var/portal-api/static.');
     return configDir;
 };
 
@@ -31,8 +34,10 @@ envReader.getInitialConfigPath = function () {
 envReader.guessServiceUrl = function (defaultHost, defaultPort) {
     var url = 'http://' + defaultHost + ':' + defaultPort + '/';
     // Are we not running on Linux? Then guess we're in local development mode.
-    if (os.type() != 'Linux')
-        url = 'http://localhost:' + defaultPort + '/';
+    if (os.type() != 'Linux') {
+        let defaultLocalIP = getDefaultLocalIP();
+        url = 'http://' + defaultLocalIP + ':' + defaultPort + '/';
+    }
     return url;
 };
 
@@ -55,6 +60,7 @@ envReader.updateConfig = function (staticConfigPath, initialStaticConfigPath) {
 envReader.checkEnvironment = function (staticConfigPath, keyText, envName) {
     if (!keyText)
         console.log('INFO: No key was passed to checkEnvironment; can only read plain text content.');
+    console.log('Reading config from: ' + staticConfigPath);
 
     if ('default' != envName)
         loadEnvironment(staticConfigPath, keyText, envName);
@@ -62,26 +68,68 @@ envReader.checkEnvironment = function (staticConfigPath, keyText, envName) {
 
     // Assign local IP to special env var, if not running in Docker
     if (!process.env.WICKED_IN_DOCKER) {
-        var ipAddresses = getLocalIPs();
-        if (ipAddresses.length > 0) {
-            if (!process.env.LOCAL_IP)
-                process.env.LOCAL_IP = ipAddresses[0];
-            if (!process.env.LOCAL_API_HOST)
-                process.env.LOCAL_API_HOST = ipAddresses[0] + ':8000';
-            if (!process.env.LOCAL_PORTAL_HOST)
-                process.env.LOCAL_PORTAL_HOST = ipAddresses[0] + ':3000';
-            if (!process.env.LOCAL_PORTAL_URL)
-                process.env.LOCAL_PORTAL_URL = 'http://' + ipAddresses[0] + ':3000';
-            if (!process.env.LOCAL_API_URL)
-                process.env.LOCAL_API_URL = 'http://' + ipAddresses[0] + ':3001';
-        }
+        var localIpAddress = getDefaultLocalIP();
+        
+        // These variable names need to be "registered" in portal-kickstarter:utils.js 
+        if (!process.env.LOCAL_IP)
+            process.env.LOCAL_IP = localIpAddress;
+        if (!process.env.LOCAL_API_HOST)
+            process.env.LOCAL_API_HOST = localIpAddress + ':8000';
+        if (!process.env.LOCAL_PORTAL_HOST)
+            process.env.LOCAL_PORTAL_HOST = localIpAddress + ':3000';
+        if (!process.env.LOCAL_PORTAL_URL)
+            process.env.LOCAL_PORTAL_URL = 'http://' + localIpAddress + ':3000';
+        if (!process.env.LOCAL_API_URL)
+            process.env.LOCAL_API_URL = 'http://' + localIpAddress + ':3001';
     }
 
     // Sanity check: Does PORTAL_API_STATIC_CONFIG match staticConfigPath?
     // If not, you have some configuration mismatch you should check on.
     if (process.env.PORTAL_API_STATIC_CONFIG != staticConfigPath)
-        throw new Error('portal-env:checkEnvironment() - The environment variable PORTAL_API_STATIC_CONFIG does not match the resolved configuration path. Please change the environment files, or pass in the correct path to the static configuration using a pre-set environment variable PORTAL_API_STATIC_CONFIG.');
+        throw new Error('portal-env:checkEnvironment() - The environment variable PORTAL_API_STATIC_CONFIG does not match the resolved configuration path. Please change the environment files, or pass in the correct path to the static configuration using a pre-set environment variable PORTAL_API_STATIC_CONFIG: ' + process.env.PORTAL_API_STATIC_CONFIG + ' vs. ' + staticConfigPath);
 };
+
+function hasEnvVars(s) {
+    return s.startsWith('$') || (s.indexOf('${') >= 0);
+}
+
+function replaceEnvVarsInString(s) {
+    var tempString = "" + s;
+    var foundVar = hasEnvVars(tempString);
+    var iterCount = 0;
+    while (foundVar) {
+        iterCount++;
+        if (iterCount > 10) {
+            console.error('Detected recursive use of env variables.');
+            console.error('Original string: ' + s);
+            console.error('Current string : ' + tempString);
+            return tempString;
+        }
+        if (tempString.startsWith('$') &&
+            !tempString.startsWith("${")) {
+            let envVarName = tempString.substring(1);
+            if (process.env[envVarName]) {
+                debug('Replacing ' + envVarName + ' with "' + process.env[envVarName] + '" in "' + tempString + '".' );
+                tempString = process.env[envVarName];
+            }
+        } else {
+            // Inline env var ${...}
+            var envRegExp = /\$\{([A-Za-z\_0-9]+)\}/g; // match ${VAR_NAME}
+            var match = envRegExp.exec(tempString);
+            if (match) {
+                let envVarName = match[1]; // Capture group 1
+                // Replace regexp with value of env var
+                if (process.env[envVarName]) {
+                    debug('Replacing ' + envVarName + ' with "' + process.env[envVarName] + '" in "' + tempString + '".' );
+                    tempString = tempString.replace(match[0], process.env[envVarName]);
+                }
+            }
+        }
+        // Possibly recurse
+        foundVar = hasEnvVars(tempString);
+    }
+    return tempString;
+}
 
 function loadEnvironment(staticConfigPath, keyText, envName) {
     var envFileName = path.join(staticConfigPath, 'env', envName + '.json');
@@ -102,6 +150,12 @@ function loadEnvironment(staticConfigPath, keyText, envName) {
         }
         process.env[varName] = varValue;
     }
+
+    // This has to be done already now to be able to resolve the data directories.
+    if (process.env.PORTAL_API_DYNAMIC_CONFIG)
+        process.env.PORTAL_API_DYNAMIC_CONFIG = replaceEnvVarsInString(process.env.PORTAL_API_DYNAMIC_CONFIG);  
+    if (process.env.PORTAL_API_STATIC_CONFIG)
+        process.env.PORTAL_API_STATIC_CONFIG = replaceEnvVarsInString(process.env.PORTAL_API_STATIC_CONFIG);  
 }
 
 envReader.sanityCheckDir = function (dirName) {
@@ -138,6 +192,8 @@ envReader.sanityCheckDir = function (dirName) {
         if (!envVarName.startsWith('PORTAL_'))
             continue;
         if (usedVars[envVarName])
+            continue;
+        if (envVarName == 'PORTAL_API_AESKEY')
             continue;
         console.log('WARNING: Environment variable ' + envVarName + ' is defined but never used.');
     }
@@ -215,6 +271,13 @@ envReader.awaitUrl = function (url, tries, timeout, callback) {
         throw new Error('envReader.awaitUrl: callback is mandatory.');
     tryGet(url, tries, 1, timeout, callback);
 };
+
+function getDefaultLocalIP() {
+    let localIPs = getLocalIPs();
+    if (localIPs.length > 0)
+        return localIPs[0];
+    return "localhost";
+}
 
 function getLocalIPs() {
     debug('getLocalIPs()');
